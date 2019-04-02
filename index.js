6e5294f1be35587f9a34c95acc144b7629739169
Joseph <z@charmed.tech>
2019-04-02 16:34:32 -0700

    Updates

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+app.use('*', (req, res, next) => {
+  console.log('code:', req.query.code);
+  next();
+});
 app.use(express.static('./public'));
 
 app.listen(PORT, () => {
