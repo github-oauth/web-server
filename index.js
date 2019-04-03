5c7c9464f8888af64bb9bbc93af675b8e029f031
Joseph <z@charmed.tech>
2019-04-02 19:08:28 -0700

    Fix jquery url, remove unnecessary route

--- index.js
+++ index.js
@@ -3,10 +3,6 @@
 const express = require('express');
 const app = express();
 
-app.use('*', (req, res, next) => {
-  console.log('code:', req.query.code);
-  next();
-});
 app.use(express.static('./public'));
 
 app.listen(process.env.PORT, () => {
